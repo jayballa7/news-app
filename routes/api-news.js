d6425a9f6ed25f911fac31219cf6fadc6d03f5b8
chrismw <chrismw7579@gmail.com>
2020-05-09 13:55:58 -0700

    added storeLinks function

--- routes/api-news.js
+++ routes/api-news.js
@@ -1,16 +1,21 @@
 const NewsAPI = require('newsapi');
+var db = require("../models");
+
 const newsapi = new NewsAPI('75d12e4aed504da3878657856f888232');
-const seconds = 2000;
+const seconds = 1000;
+const minute = 60 * seconds;
+const hour = minute * 60;
+const hours = 12 * hour;
 
 
 
-const getData = (query, country, category, cb) => {
+const getData = (category, cb) => {
     
   newsapi.v2.topHeadlines({
-    q: query,
+    // q: query,
     category: category,
     language: 'en',
-    country: country
+    country: 'us'
       }).then(response => {
         // console.log(response.articles[0].source.name);
         // console.log(response.articles);
@@ -30,17 +35,52 @@ let refreshNews = () => {};
 const getNews = () => {
   refreshNews = setInterval(() => {
     console.log('news started');
-  },seconds);
-  // call api
+    // calls the storeLinks function to save articles to the database
+
+  },hours);
+  
 }
+
 
 const stopNews = () => {
   clearInterval(refreshNews);
   console.log('news stopped');
 }
 
+// goes through all users and adds article base on user categories
+const storeLinks = () => {
+  db.user.findAll().then(users => {
+    // cycle through users
+    for (let i = 0; i < users.data.length; i++) {
+      // add articles to savearticle based on user preference
+      let categories = users.data.categories.split(',');
+
+      for (let i = 0; i < categories.length; i++) {
+        let link = '';
+        getData(categories[i], data => {
+          link = data;
+        });
+
+        db.savedArticles.create(
+            {
+              email: users.data[i].email,
+              link: link
+            }
+          )
+      }
+     
+    }
+    
+  })
+    
+    
+}
+
+
+
 
 module.exports = {
   getNews,
   stopNews
 }
+
