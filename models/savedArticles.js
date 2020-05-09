bd15351b5f6789564e2bd2fcae180dc20f375c22
Mitchell-Johnson <mitchj9@uw.edu>
2020-05-09 10:01:26 -0700

    added sql model

---
+++ models/savedArticles.js
@@ -0,0 +1,21 @@
+// This table will hold saved/bookmarked articles for every user
+// The links will be tied to users by their email
+module.exports = function(sequelize, DataTypes) {
+  var savedArticles = sequelize.define("User", {
+    // User email, used to attach to registered users
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    // Link to the saved article
+    link: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return savedArticles;
+};  
